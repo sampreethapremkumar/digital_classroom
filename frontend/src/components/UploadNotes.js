f15892685191b263145e843ca135b96c5b74a4a4
sampreethapremkumar <sampreetha.ct22@bitsathy.ac.in>
2026-01-09 19:10:00 +0530

    Fix UploadNotes to use configured axios instance

--- frontend/src/components/UploadNotes.js
+++ frontend/src/components/UploadNotes.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import axios from '../api';
 import { useNavigate } from 'react-router-dom';
 
 const UploadNotes = () => {
@@ -175,7 +175,7 @@ const UploadNotes = () => {
             uploadData.append('accessType', formData.accessType);
             uploadData.append('assignedStudents', JSON.stringify(formData.assignedStudents));
 
-            await axios.post('http://localhost:8080/api/notes', uploadData, {
+            await axios.post('/api/notes', uploadData, {
                 headers: { 'Content-Type': 'multipart/form-data' }
             });
 
